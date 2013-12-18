33f555624b01b9f4cc882a2b63510055c0750f7d
Glenn Y. Rolland <glenux@glenux.net>
2013-12-18 01:25:37 +0100

    Added base JS libs.

---
+++ assets/javascripts/app.js
@@ -0,0 +1,11 @@
+//= require 'jquery-1.10.2.js'
+//x require_tree .
+
+(function () {
+	"use strict";
+
+	$(document).ready(function() {
+		console.log('hello');
+	});
+}());
+
--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-//= require_tree .
-
-(function () {
-	"use strict";
-}());
-
