1563faec29b3b62b7801dded264ade1d7525c3ae
Glenn Y. Rolland <glenux@glenux.net>
2013-12-18 01:26:01 +0100

    Added base components for JS.

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,11 +1,28 @@
-//= require 'jquery-1.10.2.js'
+//= require 'jquery-1.10.2'
+//= require 'keymaster-1.6.1'
 //x require_tree .
 
-(function () {
+/*jslint browser: true, devel: true, white: true */
+/*global document, console, jQuery, key */
+(function (global, $) {
 	"use strict";
 
-	$(document).ready(function() {
+	$(document).ready(function () {
 		console.log('hello');
 	});
-}());
+
+	key('a', function() {
+		console.log('Add');
+	});
+
+	key('enter', function() {
+		console.log('Pressed enter');
+	});
+
+	key('c', function() {
+		console.log('Add Center');
+	});
+	// nodejs tweak
+	// if(typeof module !== 'undefined') module.exports = key;
+}(this, jQuery, key));
 
